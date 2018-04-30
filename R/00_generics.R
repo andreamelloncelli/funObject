2cecccaf6061b2ac9e14e10dd787a4052880cdb7
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2018-05-01 00:35:52 +0200

    better generics

--- R/00_generics.R
+++ R/00_generics.R
@@ -1,6 +1,6 @@
-# myNotImplemented <- function() {
-#  	stop("MyNotImplemented")
-# }
+myNotImplemented <- function() {
+ 	stop("MyNotImplemented")
+}
 setGeneric("map", function(this, f) standardGeneric("map"))
 setGeneric("lift", function(this, f) standardGeneric("lift"))
 setGeneric("flatMap", function(this, f) standardGeneric("flatMap"))
